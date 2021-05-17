4860b1021ca60bb7968cb42a393387f5391024f3
blm <bylamacq@gmail.com>
2021-05-17 21:53:11 +0200

    Small format change : tab to 4 spaces

--- KeyFinderLib/KeyFinder.h
+++ KeyFinderLib/KeyFinder.h
@@ -15,50 +15,50 @@ private:
 
     KeySearchDevice *_device;
 
-	unsigned int _compression;
+    unsigned int _compression;
 
-	std::set<KeySearchTarget> _targets;
+    std::set<KeySearchTarget> _targets;
 
-	uint64_t _statusInterval;
+    uint64_t _statusInterval;
 
     secp256k1::uint256 _stride = 1;
-	uint64_t _iterCount;
-	uint64_t _total;
-	uint64_t _totalTime;
+    uint64_t _iterCount;
+    uint64_t _total;
+    uint64_t _totalTime;
 
     secp256k1::uint256 _startKey;
     secp256k1::uint256 _endKey;
 
-	// Each index of each thread gets a flag to indicate if it found a valid hash
-	bool _running;
+    // Each index of each thread gets a flag to indicate if it found a valid hash
+    bool _running;
 
-	void(*_resultCallback)(KeySearchResult);
-	void(*_statusCallback)(KeySearchStatus);
+    void(*_resultCallback)(KeySearchResult);
+    void(*_statusCallback)(KeySearchStatus);
 
 
-	static void defaultResultCallback(KeySearchResult result);
-	static void defaultStatusCallback(KeySearchStatus status);
+    static void defaultResultCallback(KeySearchResult result);
+    static void defaultStatusCallback(KeySearchStatus status);
 
-	void removeTargetFromList(const unsigned int value[5]);
-	bool isTargetInList(const unsigned int value[5]);
-	void setTargetsOnDevice();
+    void removeTargetFromList(const unsigned int value[5]);
+    bool isTargetInList(const unsigned int value[5]);
+    void setTargetsOnDevice();
 
 public:
 
     KeyFinder(const secp256k1::uint256 &startKey, const secp256k1::uint256 &endKey, int compression, KeySearchDevice* device, const secp256k1::uint256 &stride);
 
-	~KeyFinder();
+    ~KeyFinder();
 
-	void init();
-	void run();
-	void stop();
+    void init();
+    void run();
+    void stop();
 
-	void setResultCallback(void(*callback)(KeySearchResult));
-	void setStatusCallback(void(*callback)(KeySearchStatus));
-	void setStatusInterval(uint64_t interval);
+    void setResultCallback(void(*callback)(KeySearchResult));
+    void setStatusCallback(void(*callback)(KeySearchStatus));
+    void setStatusInterval(uint64_t interval);
 
-	void setTargets(std::string targetFile);
-	void setTargets(std::vector<std::string> &targets);
+    void setTargets(std::string targetFile);
+    void setTargets(std::vector<std::string> &targets);
 
     secp256k1::uint256 getNextKey();
 };
